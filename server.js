2fb65d5f7dc916e1f102ff3121f987fb6bfc1316
adeeb jamil <2100032535@kluniversity.in>
2025-02-27 18:09:25 +0530

    up

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 
 import connectToMongoDB from './db/connectToMongoDB.js';
-import { app, server  } from './socket/socket.js';
-
+import { app, server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 5000;
 
@@ -18,18 +17,20 @@ const __dirname = path.resolve();
 
 dotenv.config();
 app.use(express.json()); // to parse the incoming request with JSON payloads from req.body
-app.use(cookieParser()); 
+app.use(cookieParser());
 
-app.use("/api/auth",authRoutes)
-app.use('/api/messages',messageRoutes);
-app.use('/api/users',userRoutes);
+app.use("/api/auth", authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// Serve static files from the client/dist directory
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('*', (req, res) => {res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 server.listen(PORT, () => {
-  connectToMongoDB()
-  console.log(`Server is running on ports ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
 });
